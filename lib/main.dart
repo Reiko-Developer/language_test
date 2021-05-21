59a6e03228e5c415bd99d7cb7c9b6691e4a8010e
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-05-21 19:41:21 -0300

    Implements InnerShadow (Neumorphic style)

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,105 @@ class MyApp extends StatelessWidget {
         body: Container(
           width: double.infinity,
           height: double.infinity,
-          child: OuterShadow(),
+          // color: Colors.grey[500],
+          child: Neumorphic(),
         ),
+      ),
+    );
+  }
+}
+
+class Neumorphic extends StatefulWidget {
+  @override
+  _NeumorphicState createState() => _NeumorphicState();
+}
+
+class _NeumorphicState extends State<Neumorphic> {
+  final Color dividerColor = const Color(0xFFBDBDBD);
+  final backgroundColor = Colors.grey[300];
+
+  //any number, appears to not change nothing on the ui.
+  double unit = 200;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: [
+          //OuterShadows
+          Container(
+            width: 300,
+            height: 300,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: backgroundColor,
+                boxShadow: [
+                  BoxShadow(
+                    //darkmode
+                    // color: Colors.white.withOpacity(0.2),
+                    color: Colors.white,
+                    offset: Offset(-unit / 2, -unit / 2),
+                    blurRadius: 1.5 * unit,
+                  ),
+                  BoxShadow(
+                    color: dividerColor,
+                    offset: Offset(unit / 2, unit / 2),
+                    blurRadius: 1.5 * unit,
+                  ),
+                ],
+              ),
+              child: Center(
+                child: SizedBox(
+                  width: 280,
+                  height: 280,
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: backgroundColor,
+                          gradient: RadialGradient(
+                            colors: [
+                              // darkMode
+                              // backgroundColor.withOpacity(0.0)
+                              Colors.white.withOpacity(0.0),
+                              dividerColor,
+                            ],
+                            center: AlignmentDirectional(0.1, 0.1),
+                            focal: AlignmentDirectional(0.0, 0.0),
+                            radius: 0.65,
+                            focalRadius: 0.001,
+                            stops: [0.3, 1.0],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: backgroundColor,
+                          gradient: RadialGradient(
+                            colors: [
+                              Colors.white.withOpacity(0.0),
+                              //darkMode:
+                              //Colors.white.withOpacity(0.3)
+                              Colors.white,
+                            ],
+                            center: AlignmentDirectional(-0.1, -0.1),
+                            focal: AlignmentDirectional(0.0, 0.0),
+                            radius: 0.67,
+                            focalRadius: 0.001,
+                            stops: [0.75, 1.0],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
